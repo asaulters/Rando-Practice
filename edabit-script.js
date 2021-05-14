0a8f3824d1d161cea7f0bc3a5375da690fa04a86
asaulters <71015650+asaulters@users.noreply.github.com>
2021-05-13 21:14:04 -0400

    Add files via upload

---
+++ edabit-script.js
@@ -0,0 +1,12 @@
+function printArray(number) {
+  var newArray = [];
+
+  for (var i = 1; i <= number; i++) {
+    newArray.push(i);
+  }
+
+  console.log(newArray);
+}
+
+let a = [1, 2, 3, 4, 5, 6, 7, 8];
+printArray(2);
