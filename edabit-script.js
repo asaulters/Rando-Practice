cc933b87e42550d3c440dd18fab73b20710d477f
asaulters <71015650+asaulters@users.noreply.github.com>
2021-07-01 21:19:51 -0400

    Add files via upload

--- edabit-script.js
+++ edabit-script.js
@@ -407,3 +407,21 @@ function valueAt(arr, i) {
   let a = arr[i];
   return a;
 }
+
+// A bartender is writing a simple program to determine whether he should serve drinks to someone. He only serves drinks to people 18 and older and when he's not on break.
+// Given the person's age, and whether break time is in session, create a function which returns whether he should serve drinks.
+//is age >= 18
+// is he on break
+function shouldServeDrinks(age, onBreak) {
+  if (age > 17 && onBreak == false) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+// Write a function to reverse an array.
+function reverse(arr) {
+  let a = arr;
+  return a.reverse();
+}
